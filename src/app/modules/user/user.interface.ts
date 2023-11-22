9655d70241231833d418ba6f4796b07127f57218
himel <himelku1@gmail.com>
2023-11-22 23:47:37 +0600

    user interface created

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,30 @@
+export interface FullName {
+    firstName: string;
+    lastName: string;
+}
+
+export interface Address {
+    street: string;
+    city: string;
+    country: string;
+}
+
+export interface Order {
+    productName: string;
+    price: number;
+    quantity: number;
+}
+
+export interface User {
+    userId: number;
+    username: string;
+    password: string;
+    fullName: FullName;
+    age: number;
+    email: string;
+    isActive: boolean;
+    hobbies: string[];
+    address: Address;
+    orders: Order[];
+}
+
